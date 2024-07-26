027e41685557ea018daa618cae35efe9484677b9
vanko010 <ld.vu000@gmail.com>
2024-07-26 16:57:20 +0700

    Push lần 4

--- lienhe.py
+++ lienhe.py
@@ -61,8 +61,6 @@ def checkhovaten():
                 noidung.clear()
                 # mbv.clear()
     driver.close()
-checkhovaten()
-
 def checksdt():
     driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
     # Mở trình duyệt ở chế độ toàn màn hình
@@ -78,39 +76,39 @@ def checksdt():
     # mbv = driver.find_element(By.XPATH,xpath.xmbv)
     btngui = driver.find_element(By.XPATH, xpath.xgui)
     btnlamlai = driver.find_element(By.XPATH, xpath.xlamlai)
-    with open("sdt.json", "r", encoding='utf-8') as file_sdt:
-        dbsdt = json.load(file_sdt)
+    with open("dbsodienthoai.json", "r", encoding='utf-8') as file_sdt:
+        db_sdt = json.load(file_sdt)
     counter = 1
-    for key,value in dbsdt:
-        vlsdt = dbsdt[value]
-        hovaten.send_keys("Lê Đoàn Vũ")
-        sdt.send_keys(vlsdt)
-        email.send_keys("test@gmail.com")
-        diachi.send_keys("344 Huynh Tan Phat, phường Bình Thuận, quận 7, TPHCM")
-        tieude.send_keys("Test Liên hệ")
-        noidung.send_keys("Test liên hệ")
-        # mbv.send_keys("1231123")
-        btngui.click()
-        time.sleep(3)
-        try:
-            alert = driver.switch_to.alert
-            if alert.text == "Họ và tên chưa hợp lệ, vui lòng nhập lại!":
-                print(f"{counter}Họ tên fail, nhập lại")
-            if alert.text == "Yêu cầu của bạn đã được gửi!":
-                print(f"{counter}Yêu cầu đã được gửi")
-            elif alert.text == "Nhập mã bảo vệ chưa đúng!":
-                print(f"{counter}Yêu cầu chưa được gửi đi")
-            else:
-                print(f"{counter}Chưa nhập mã bảo vệ")
-            alert = Alert(driver)
-            alert.accept()
-        except:
-            print(f"{counter}Case ngoại lệ, check lại")
-        counter += 1
-        hovaten.clear()
-        sdt.clear()
-        email.clear()
-        diachi.clear()
-        tieude.clear()
-        noidung.clear()
-        # mbv.clear()
+    for dbsdt in db_sdt:
+        for key,value in dbsdt.items():
+            vlsdt = dbsdt["sodienthoai"]
+            hovaten.send_keys("Lê Đoàn Vũ")
+            sdt.send_keys(vlsdt)
+            email.send_keys("test@gmail.com")
+            diachi.send_keys("344 Huynh Tan Phat, phường Bình Thuận, quận 7, TPHCM")
+            tieude.send_keys("Test Liên hệ")
+            noidung.send_keys("Test liên hệ")
+            # mbv.send_keys("1231123")
+            btngui.click()
+            try:
+                alert = driver.switch_to.alert
+                if alert.text == "Số điện thoại chưa hợp lệ, vui lòng nhập lại!":
+                    print(f"Case {counter} Số điện thoại fail, nhập lại")
+                if alert.text == "Yêu cầu của bạn đã được gửi!":
+                    print(f"Case {counter} Yêu cầu đã được gửi")
+                elif alert.text == "Nhập mã bảo vệ chưa đúng!":
+                    print(f"Case {counter} Yêu cầu chưa được gửi đi")
+                else:
+                    print(f"Case {counter} Chưa nhập mã bảo vệ")
+                alert = Alert(driver)
+                alert.accept()
+            except:
+                print(f"Case {counter} ngoại lệ, check lại")
+            counter += 1
+            hovaten.clear()
+            sdt.clear()
+            email.clear()
+            diachi.clear()
+            tieude.clear()
+            noidung.clear()
+            # mbv.clear()
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import login
 import lienhe
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    login.login()
-    print ("===========================")
-    lienhe.checkhovaten()
+    # print("Check liên hệ - Họ và tên")
+    # lienhe.checkhovaten()
+    # print ("Check login admin")
+    # login.login()
+    print("Check liên - Số điện thoại")
+    lienhe.checksdt()
