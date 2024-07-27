ea174907ab0e851bc2825f3690d5df20e248fea6
vanko010 <ld.vu000@gmail.com>
2024-07-27 11:01:00 +0700

    Push done lienhe 27/07

--- lienhe.py
+++ lienhe.py
@@ -4,119 +4,174 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 import json
 import xpath
 
-def checkhovaten():
+def setup_driver():
     # #Code chạy linux
-    # driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 
     #Code chạy window
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.maximize_window()
     driver.get("https://webdemo5.pavietnam.vn/2020_hctechco/lien-he")
-    with open("datahovaten.json", "r", encoding='utf-8') as file_hovaten:
-        dbhovaten = json.load(file_hovaten)
-    hovaten = driver.find_element(By.XPATH, xpath.xhovaten)
-    sdt = driver.find_element(By.XPATH, xpath.xsdt)
-    email = driver.find_element(By.XPATH,xpath.xemail)
-    diachi = driver.find_element(By.XPATH,xpath.xdiachi)
-    tieude = driver.find_element(By.XPATH,xpath.xtieude)
-    noidung = driver.find_element(By.XPATH,xpath.xnoidung)
-    # mbv = driver.find_element(By.XPATH,xpath.xmbv)
-    btngui = driver.find_element(By.XPATH,xpath.xgui)
-    btnlamlai = driver.find_element(By.XPATH,xpath.xlamlai)
+    return driver
+def load_file(fileload):
+    with open(fileload, "r", encoding='utf-8') as file_hovaten:
+        return json.load(file_hovaten)
+def fill_hovaten(driver, data):
+    driver.find_element(By.XPATH, xpath.xhovaten).send_keys(data["hovaten"])
+    driver.find_element(By.XPATH, xpath.xsdt).send_keys("0987654321")
+    driver.find_element(By.XPATH, xpath.xemail).send_keys("test@gmail.com")
+    driver.find_element(By.XPATH, xpath.xdiachi).send_keys("344 Huynh Tan Phat, phường Bình Thuận, quận 7, TPHCM")
+    driver.find_element(By.XPATH, xpath.xtieude).send_keys("Test Liên hệ")
+    driver.find_element(By.XPATH, xpath.xnoidung).send_keys("Test liên hệ")
+    # driver.find_element(By.XPATH, xpath.xmbv).send_keys("1231123")
+def submit_form(driver):
+    driver.find_element(By.XPATH,xpath.xgui).click()
+    # driver.find_element(By.XPATH,xpath.xlamlai).click()
+def check_alert(driver,counter):
+    try:
+        alert = driver.switch_to.alert
+        if alert.text == "Họ và tên chưa hợp lệ, vui lòng nhập lại!":
+            print(f"Case {counter} Họ tên fail, nhập lại")
+        elif alert.text == "Yêu cầu của bạn đã được gửi!":
+            print(f"Case {counter} Yêu cầu đã được gửi")
+        elif alert.text == "Nhập mã bảo vệ chưa đúng!":
+            print(f"Case {counter} Yêu cầu chưa được gửi đi")
+        else:
+            print(f"Case {counter} Chưa nhập mã bảo vệ")
+            alert = Alert(driver)
+            alert.accept()
+    except:
+        print(f"{counter}Case ngoại lệ, check lại")
+def clear_data(driver):
+    driver.find_element(By.XPATH, xpath.xhovaten).clear()
+    driver.find_element(By.XPATH, xpath.xsdt).clear()
+    driver.find_element(By.XPATH, xpath.xemail).clear()
+    driver.find_element(By.XPATH, xpath.xdiachi).clear()
+    driver.find_element(By.XPATH, xpath.xtieude).clear()
+    driver.find_element(By.XPATH, xpath.xnoidung).clear()
+def checkhovaten():
+    driver = setup_driver()
+    dbhovaten = load_file ("datahovaten.json")
     counter = 1
     for data in dbhovaten:
-            for key,value in data.items():
-                ten = data["hovaten"]
-                hovaten.send_keys(ten)
-                sdt.send_keys("0987654321")
-                email.send_keys("test@gmail.com")
-                diachi.send_keys("344 Huynh Tan Phat, phường Bình Thuận, quận 7, TPHCM")
-                tieude.send_keys("Test Liên hệ")
-                noidung.send_keys("Test liên hệ")
-                # mbv.send_keys("1231123")
-                btngui.click()
+                fill_hovaten(driver,data)
+                submit_form(driver)
                 time.sleep(1)
-                try:
-                    alert = driver.switch_to.alert
-                    if alert.text == "Họ và tên chưa hợp lệ, vui lòng nhập lại!":
-                        print(f"Case {counter} Họ tên fail, nhập lại")
-                    elif alert.text == "Yêu cầu của bạn đã được gửi!":
-                        print(f"Case {counter} Yêu cầu đã được gửi")
-                    elif alert.text == "Nhập mã bảo vệ chưa đúng!":
-                        print(f"Case {counter} Yêu cầu chưa được gửi đi")
-                    else:
-                        print(f"Case {counter} Chưa nhập mã bảo vệ")
-                    alert = Alert(driver)
-                    alert.accept()
-                except:
-                    print(f"{counter}Case ngoại lệ, check lại")
+                check_alert(driver,counter)
+                clear_data(driver)
                 counter += 1
-                hovaten.clear()
-                sdt.clear()
-                email.clear()
-                diachi.clear()
-                tieude.clear()
-                noidung.clear()
-                # mbv.clear()
-    driver.close()
-def checksdt():
-    # #Code chạy linux
-    # driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
-    # driver.maximize_window()
-    # driver.get("https://webdemo5.pavietnam.vn/2020_hctechco/lien-he")
 
-    #Code chạy window
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.maximize_window()
-    driver.get("https://webdemo5.pavietnam.vn/2020_hctechco/lien-he")
-    hovaten = driver.find_element(By.XPATH, xpath.xhovaten)
-    sdt = driver.find_element(By.XPATH, xpath.xsdt)
-    email = driver.find_element(By.XPATH, xpath.xemail)
-    diachi = driver.find_element(By.XPATH, xpath.xdiachi)
-    tieude = driver.find_element(By.XPATH, xpath.xtieude)
-    noidung = driver.find_element(By.XPATH, xpath.xnoidung)
-    # mbv = driver.find_element(By.XPATH,xpath.xmbv)
-    btngui = driver.find_element(By.XPATH, xpath.xgui)
-    btnlamlai = driver.find_element(By.XPATH, xpath.xlamlai)
-    with open("dbsodienthoai.json", "r", encoding='utf-8') as file_sdt:
-        db_sdt = json.load(file_sdt)
+    driver.close()
+
+def fill_sdt(driver,data):
+    driver.find_element(By.XPATH, xpath.xhovaten).send_keys("Lê Đoàn Vũ")
+    driver.find_element(By.XPATH, xpath.xsdt).send_keys(data["sodienthoai"])
+    driver.find_element(By.XPATH, xpath.xemail).send_keys("test@gmail.com")
+    driver.find_element(By.XPATH, xpath.xdiachi).send_keys("344 Huynh Tan Phat, phường Bình Thuận, quận 7, TPHCM")
+    driver.find_element(By.XPATH, xpath.xtieude).send_keys("Test Liên hệ")
+    driver.find_element(By.XPATH, xpath.xnoidung).send_keys("Test liên hệ")
+    # driver.find_element(By.XPATH, xpath.xmbv).send_keys("1231123")
+def checksdt():
+    driver= setup_driver()
+    dbsdt = load_file("dbsodienthoai.json")
     counter = 1
-    for dbsdt in db_sdt:
-        for key,value in dbsdt.items():
-            vlsdt = dbsdt["sodienthoai"]
-            hovaten.send_keys("Lê Đoàn Vũ")
-            sdt.send_keys(vlsdt)
-            email.send_keys("test@gmail.com")
-            diachi.send_keys("344 Huynh Tan Phat, phường Bình Thuận, quận 7, TPHCM")
-            tieude.send_keys("Test Liên hệ")
-            noidung.send_keys("Test liên hệ")
-            # mbv.send_keys("1231123")
-            btngui.click()
-            try:
-                alert = driver.switch_to.alert
-                if alert.text == "Số điện thoại chưa hợp lệ, vui lòng nhập lại!":
-                    print(f"Case {counter} Số điện thoại fail, nhập lại")
-                if alert.text == "Yêu cầu của bạn đã được gửi!":
-                    print(f"Case {counter} Yêu cầu đã được gửi")
-                elif alert.text == "Nhập mã bảo vệ chưa đúng!":
-                    print(f"Case {counter} Yêu cầu chưa được gửi đi")
-                else:
-                    print(f"Case {counter} Chưa nhập mã bảo vệ")
-                alert = Alert(driver)
-                alert.accept()
-            except:
-                print(f"Case {counter} ngoại lệ, check lại")
-            counter += 1
-            hovaten.clear()
-            sdt.clear()
-            email.clear()
-            diachi.clear()
-            tieude.clear()
-            noidung.clear()
-            # mbv.clear()
-    driver.quit()
+    for data in dbsdt:
+        fill_sdt(driver,data)
+        submit_form(driver)
+        time.sleep(1)
+        check_alert(driver,counter)
+        clear_data(driver)
+        counter +=1
+    driver.close()
+
+def fill_email(driver,data):
+    driver.find_element(By.XPATH, xpath.xhovaten).send_keys("Lê Đoàn Vũ")
+    driver.find_element(By.XPATH, xpath.xsdt).send_keys("0987654321")
+    driver.find_element(By.XPATH, xpath.xemail).send_keys(data["email"])
+    driver.find_element(By.XPATH, xpath.xdiachi).send_keys("344 Huynh Tan Phat, phường Bình Thuận, quận 7, TPHCM")
+    driver.find_element(By.XPATH, xpath.xtieude).send_keys("Test Liên hệ")
+    driver.find_element(By.XPATH, xpath.xnoidung).send_keys("Test liên hệ")
+    # driver.find_element(By.XPATH, xpath.xmbv).send_keys("1231123")
+
+def checkemail():
+    driver = setup_driver()
+    dbemail = load_file("dbemail.json")
+    counter = 1
+    for data in dbemail:
+        fill_email(driver,data)
+        submit_form(driver)
+        time.sleep(1)
+        check_alert(driver, counter)
+        clear_data(driver)
+        counter += 1
+    driver.close()
+
+def fill_diachi(driver, data):
+    driver.find_element(By.XPATH, xpath.xhovaten).send_keys("Lê Đoàn Vũ")
+    driver.find_element(By.XPATH, xpath.xsdt).send_keys("0987654321")
+    driver.find_element(By.XPATH, xpath.xemail).send_keys("test@gmail.com")
+    driver.find_element(By.XPATH, xpath.xdiachi).send_keys(data["diachi"])
+    driver.find_element(By.XPATH, xpath.xtieude).send_keys("Test Liên hệ")
+    driver.find_element(By.XPATH, xpath.xnoidung).send_keys("Test liên hệ")
+    # driver.find_element(By.XPATH, xpath.xmbv).send_keys("1231123")
+
+def checkdiachi():
+    driver = setup_driver()
+    dbdiachi = load_file("dbdiachi.json")
+    counter = 1
+    for data in dbdiachi:
+        fill_diachi(driver,data)
+        submit_form(driver)
+        time.sleep(1)
+        check_alert(driver,counter)
+        clear_data(driver)
+        counter += 1
+    driver.close()
+
+def fill_tieude(driver, data):
+    driver.find_element(By.XPATH, xpath.xhovaten).send_keys("Lê Đoàn Vũ")
+    driver.find_element(By.XPATH, xpath.xsdt).send_keys("0987654321")
+    driver.find_element(By.XPATH, xpath.xemail).send_keys("test@gmail.com")
+    driver.find_element(By.XPATH, xpath.xdiachi).send_keys("344 Huynh Tan Phat, phường Bình Thuận, quận 7, TPHCM")
+    driver.find_element(By.XPATH, xpath.xtieude).send_keys(data["tieude"])
+    driver.find_element(By.XPATH, xpath.xnoidung).send_keys("Test liên hệ")
+    # driver.find_element(By.XPATH, xpath.xmbv).send_keys("1231123")
+
+def checktieude():
+    driver = setup_driver()
+    dbtieude = load_file("dbtieude.json")
+    counter = 1
+    for data in dbtieude:
+        fill_tieude(driver,data)
+        submit_form(driver)
+        time.sleep(1)
+        check_alert(driver,counter)
+        clear_data(driver)
+        counter += 1
+    driver.close()
+
+def fill_noidung(driver, data):
+    driver.find_element(By.XPATH, xpath.xhovaten).send_keys("Lê Đoàn Vũ")
+    driver.find_element(By.XPATH, xpath.xsdt).send_keys("0987654321")
+    driver.find_element(By.XPATH, xpath.xemail).send_keys("test@gmail.com")
+    driver.find_element(By.XPATH, xpath.xdiachi).send_keys("344 Huynh Tan Phat, phường Bình Thuận, quận 7, TPHCM")
+    driver.find_element(By.XPATH, xpath.xtieude).send_keys("Test liên hệ")
+    driver.find_element(By.XPATH, xpath.xnoidung).send_keys(data["noidung"])
+    # driver.find_element(By.XPATH, xpath.xmbv).send_keys("1231123")
+
+def checknoidung():
+    driver = setup_driver()
+    dbnoidung = load_file("dbtieude.json")
+    counter = 1
+    for data in dbnoidung:
+        fill_noidung(driver,data)
+        submit_form(driver)
+        time.sleep(1)
+        check_alert(driver,counter)
+        clear_data(driver)
+        counter += 1
+    driver.close()
--- login.py
+++ login.py
@@ -4,16 +4,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 import xpath
 import json
 
 def login():
-    # #Code chạy linux 
-    # driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+    # Code chạy linux
+    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
     
     #Code chạy window
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     
     driver.get("https://webdemo5.pavietnam.vn/2020_hctechco/myadmin/index.php?module=login")
     #load data tài khoản
@@ -22,29 +22,30 @@ def login():
     login_true = "Trang quản trị"
     counter = 1
     for data in db:
-        for key, value in data.items():
             tk = data["user"]
             mk = data["password"]
             email = driver.find_element(By.XPATH, xpath.xuser)
+            email.clear()
             email.send_keys(tk)
             pw = driver.find_element(By.XPATH, xpath.xpw)
+            pw.clear()
             pw.send_keys(mk)
             btnlogin = driver.find_element(By.XPATH, xpath.btnlogin)
             btnlogin.click()
             try:
                 title = driver.title
                 if title == login_true:
-                    if counter == 35:
+                    if counter == 18:
                          print(f"Case {counter} true -> Passed")
                     else:
                         print (f"Case {counter} là bug -> Check lại")
                     break
                 else:
-                    time.sleep(2) 
+                    time.sleep(1)
                     alert = Alert(driver)
                     alert.accept()
             except Exception as e:
-                if counter != 35:
+                if counter != 18:
                     print(f"Case {counter} check login fail")
                     print("-> Passed")
                 else:
